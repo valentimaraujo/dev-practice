e14ecdc149befc00f4cdf3e6fcab0c237df72f0d
Valentim Vieira de Araújo <varaujo@inone.com.br>
2020-01-10 18:26:17 -0300

    Update

--- jwt-authentication-authorization-in-nodejs-express-mongodb-rest-apis/src/db/db.js
+++ jwt-authentication-authorization-in-nodejs-express-mongodb-rest-apis/src/db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URL, {
-mongoose.connect('mongodb+srv://new-evodev:ghetto-v5821@evodev-services-ge9wc.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
+    useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', err => {
+    console.error(`MongoDB connection error: ${err}`);
 });
